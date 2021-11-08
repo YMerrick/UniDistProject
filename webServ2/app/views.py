35bcc257e6183d629f4968134e1a5883f417b481
Thomas Irwin <sc19ti@leeds.ac.uk>
2021-11-08 14:42:00 +0000

    boilerplate code for flask app copied across from webServ1

---
+++ webServ2/app/views.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from app import app
+from flask_restful import Resource, Api
+
+def testRun():
+    global app
+    app = Flask(__name__)
+    api = Api(app)
+    
+if __name__ == '__main__':
+    testRun()    
+
+print(__name__)
+
+message = [{'message': ' Hello World '}]
+
+@app.route("/")
+def hello_world():
+    return jsonify(message)
+
+@app.route("/", methods= ['POST'])
+def add_message():
+    message.append(request.get_json())
+    return '',204
