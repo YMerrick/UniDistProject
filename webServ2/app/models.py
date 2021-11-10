4cd9b938a0ce0673206ea14685e94bfe05bbcb93
Thomas Irwin <sc19ti@leeds.ac.uk>
2021-11-10 17:52:42 +0000

    implemented second web service

--- webServ2/app/models.py
+++ webServ2/app/models.py
@@ -1,15 +1,41 @@
-from app import app
+from app import db
 from flask_sqlalchemy import SQLAlchemy
-
-
 
 class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), index=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
 
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    songs = db.relationship('Song', backref='artist', lazy='dynamic', primaryjoin = (Song.artist_id == id))
     name = db.Column(db.String(200), index=True)
-    songs = db.Relationship('Song', backref = 'Song', lazy = 'dynamic')
 
+#code to create and populate database
+"""db.create_all()
+
+a = Artist(name = "Rick Astley")
+db.session.add(a)
+db.session.commit()
+
+b = Artist.query.get(1)
+s = Song(title = "Never Gonna Give You Up", artist_id = b.id)
+db.session.add(s)
+
+a = Artist(name = "Queen")
+db.session.add(a)
+db.session.commit()
+
+b = Artist.query.get(2)
+s = Song(title = "I Want To Break Free", artist_id = b.id)
+db.session.add(s)
+
+a = Artist(name = "Beatles")
+db.session.add(a)
+db.session.commit()
+
+b = Artist.query.get(3)
+s = Song(title = "Hey Jude", artist_id = b.id)
+db.session.add(s)
+
+db.session.commit()"""
--- webServ2/app/views.py
+++ webServ2/app/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
-from app import app
+from app import app, models
 from flask_restful import Resource, Api
+#import requests
 
 def testRun():
     global app
@@ -13,19 +14,18 @@ if __name__ == '__main__':
 
 print(__name__)
 
-message = [{'message': ' Hello World '}]
-
-@app.route("/")
-def hello_world():
-    return jsonify(message)
-
-@app.route("/", methods= ['POST'])
-def add_message():
-    message.append(request.get_json())
-    return '',204
-
 class GetArtist(Resource):
-    def get(self):
-        return {'hello':'world'}
+    
+    def get(self, song):
+        song = song.replace("_", " ")
+        song_record = models.Song.query.filter_by(title=song).all()
+        artist = models.Artist.query.get(song_record[0].artist_id)
+        if not artist:
+            return "Song not found"
+        
+        #This will send the request to the external service, commented out for now while the external client has not been implemented
+        #external_client = requests.post("*url of external service*" + artist.name)
+        #for now, just return result to the user
+        return artist.name
 
-api.add_resource(GetArtist, '/hello')
+api.add_resource(GetArtist, '/song/<string:song>', methods = ['GET'])
